4d2f360d345d3ce1f995fe281b4c43c66451e81a
Alva Chien <alvachien@live.com>
2017-12-31 22:37:55 +0800

    Add Aliyun deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,34 @@ export const environment = {
   production: true,
   LoggingLevel: 1, // Crash
 
-  AlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/album',
-  PhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photo',
-  PhotoFileAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photofile',
-  AlbumPhotoByAlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyalbum',
-  AlbumPhotoByPhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyphoto',
-  AlbumPhotoLinkUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotolink',
-  PublicPhotoInAPIBaseUrl: 'http://acgalleryapi.azurewebsites.net/updphoto',
+  AlbumAPIUrl: 'http://118.178.58.187:5310/api/album',
+  PhotoAPIUrl: 'http://118.178.58.187:5310/api/photo',
+  PhotoFileAPIUrl: 'http://118.178.58.187:5310/api/photofile',
+  AlbumPhotoByAlbumAPIUrl: 'http://118.178.58.187:5310/api/albumphotobyalbum',
+  AlbumPhotoByPhotoAPIUrl: 'http://118.178.58.187:5310/api/albumphotobyphoto',
+  AlbumPhotoLinkUrl: 'http://118.178.58.187:5310/api/albumphotolink',
+  PublicPhotoInAPIBaseUrl: 'http://118.178.58.187:5310/updphoto',
 
-  IDServerUrl: 'http://acidserver.azurewebsites.net/',
-  AppLoginCallbackUrl: 'http://acgallery.azurewebsites.net/logincallback.html',
-  AppLogoutCallbackUrl: 'http://acgallery.azurewebsites.net/index.html',
-  AppHost: 'http://acgallery.azurewebsites.net'
+  IDServerUrl: 'http://118.178.58.187:5100/',
+  AppLoginCallbackUrl: 'http://118.178.58.187:5210/logincallback.html',
+  AppLogoutCallbackUrl: 'http://118.178.58.187:5210/index.html',
+  AppHost: 'http://118.178.58.187:5210'
 };
+
+// export const environment = {
+//   production: true,
+//   LoggingLevel: 1, // Crash
+
+//   AlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/album',
+//   PhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photo',
+//   PhotoFileAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photofile',
+//   AlbumPhotoByAlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyalbum',
+//   AlbumPhotoByPhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyphoto',
+//   AlbumPhotoLinkUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotolink',
+//   PublicPhotoInAPIBaseUrl: 'http://acgalleryapi.azurewebsites.net/updphoto',
+
+//   IDServerUrl: 'http://acidserver.azurewebsites.net/',
+//   AppLoginCallbackUrl: 'http://acgallery.azurewebsites.net/logincallback.html',
+//   AppLogoutCallbackUrl: 'http://acgallery.azurewebsites.net/index.html',
+//   AppHost: 'http://acgallery.azurewebsites.net'
+// };
