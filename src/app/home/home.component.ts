db889e1baf33726226c8acd40dbbaed469d9ac56
Alva Chien <alvachien@163.com>
2018-01-02 19:08:36 +0800

    Add new codes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { AuthService } from '../services';
+import { LogLevel } from '../model';
 
 @Component({
   selector: 'acgallery-home',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public btnLoginTxt = '';
 
-  constructor() { }
+  constructor(private _authService: AuthService,
+    private _zone: NgZone) {
+    this._authService.authContent.subscribe(x => {
+      this._zone.run(() => {
+        if (x.isAuthorized) {
+          this.btnLoginTxt = 'Login.UserDetail';
+        } else {
+          this.btnLoginTxt = 'Login.Login';
+        }
+      });
+    }, error => {
+      if (environment.LoggingLevel >= LogLevel.Error) {
+        console.error('ACGallery [Error]: Failed in subscribe to User', error);
+      }
+    }, () => {
+      // Completed
+    });
+  }
 
   ngOnInit() {
   }
