7ebf50aead3bc005c8aa160f5dde0564e21407e8
Alva Chien <alvachien@live.com>
2018-05-21 21:29:28 +0800

    Add new codes

--- src/app/services/uistatus.service.ts
+++ src/app/services/uistatus.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Injectable()
 export class UIStatusService {
@@ -6,6 +8,20 @@ export class UIStatusService {
   public elemPSWP: any;
   public selPhotoInAblum: any;
   public selPhotoInPhotoList: any;
+  isHandset: Observable<BreakpointState>;
+  isTablet: Observable<BreakpointState>;
+  isWeb: Observable<BreakpointState>;
+  isPortrait: Observable<BreakpointState>;
+  isLandscape: Observable<BreakpointState>;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver
+    ) {
+      this.isHandset = this.breakpointObserver.observe([Breakpoints.HandsetLandscape,
+        Breakpoints.HandsetPortrait]);
+      this.isTablet = this.breakpointObserver.observe(Breakpoints.Tablet);
+      this.isWeb = this.breakpointObserver.observe([Breakpoints.WebLandscape,
+        Breakpoints.WebPortrait]);
+      this.isPortrait = this.breakpointObserver.observe('(orientation: portrait)');
+      this.isLandscape = this.breakpointObserver.observe('(orientation: landscape)');
+  }
 }
