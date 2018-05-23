be69855d564d85232aace3eacfa6fedbecc4d449
Alva Chien <alvachien@163.com>
2018-05-23 13:19:20 +0800

    Update environment for Azure

--- src/environments/environment.azure.ts
+++ src/environments/environment.azure.ts
@@ -2,19 +2,19 @@ export const environment = {
   production: true,
   LoggingLevel: 1, // Crash
 
-  AlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/album',
-  PhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photo',
-  PhotoFileAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/photofile',
-  AlbumPhotoByAlbumAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyalbum',
-  AlbumPhotoByPhotoAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotobyphoto',
-  AlbumPhotoLinkUrl: 'http://acgalleryapi.azurewebsites.net/api/albumphotolink',
-  PublicPhotoInAPIBaseUrl: 'http://acgalleryapi.azurewebsites.net/updphoto',
-  WakeupAPIUrl: 'http://acgalleryapi.azurewebsites.net/api/wakeup',
+  AlbumAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/album',
+  PhotoAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/photo',
+  PhotoFileAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/photofile',
+  AlbumPhotoByAlbumAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/albumphotobyalbum',
+  AlbumPhotoByPhotoAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/albumphotobyphoto',
+  AlbumPhotoLinkUrl: 'https://acgalleryapi.azurewebsites.net/api/albumphotolink',
+  PublicPhotoInAPIBaseUrl: 'https://acgalleryapi.azurewebsites.net/updphoto',
+  WakeupAPIUrl: 'https://acgalleryapi.azurewebsites.net/api/wakeup',
 
-  IDServerUrl: 'http://acidserver.azurewebsites.net',
-  AppLoginCallbackUrl: 'http://acgallery.azurewebsites.net/logincallback.html',
-  AppLogoutCallbackUrl: 'http://acgallery.azurewebsites.net',
-  AppHost: 'http://acgallery.azurewebsites.net',
-  AppHIH: 'http://achihui.azurewebsites.net',
+  IDServerUrl: 'https://acidserver.azurewebsites.net',
+  AppLoginCallbackUrl: 'https://acgallery.azurewebsites.net/logincallback.html',
+  AppLogoutCallbackUrl: 'https://acgallery.azurewebsites.net',
+  AppHost: 'https://acgallery.azurewebsites.net',
+  AppHIH: 'https://achihui.azurewebsites.net',
   AppMathExercise: 'https://alvachien.github.io/mathexercise/'
 };
