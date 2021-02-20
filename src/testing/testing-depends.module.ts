2fedeb513efa504ab2d15201efb345a7ae0baff1
Alva Chien <alvachien@live.com>
2021-02-21 00:20:43 +0800

    Make the UT pass

--- src/testing/testing-depends.module.ts
+++ src/testing/testing-depends.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UIModulesModule } from 'src/app/ui-modules.module';
 import { getTranslocoModule } from './transloco-testing.module';
@@ -16,9 +17,11 @@ import { TranslocoModule, TranslocoTestingModule } from '@ngneat/transloco';
         HttpClientTestingModule,
         FormsModule,
         NoopAnimationsModule,
+        RouterTestingModule,
+
         TranslocoTestingModule,
 
-        UIModulesModule,        
+        UIModulesModule,
     ]
 })
 export class TestingDependsModule { }
